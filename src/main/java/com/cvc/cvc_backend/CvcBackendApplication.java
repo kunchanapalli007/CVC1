458750237911c70cfeb5ab9572863ce697cf4a22
prasanth <kvsprasanth007@gmail.com>
2025-05-09 04:32:07 -0400

    nm

--- src/main/java/com/cvc/cvc_backend/CvcBackendApplication.java
+++ src/main/java/com/cvc/cvc_backend/CvcBackendApplication.java
@@ -3,6 +3,7 @@ package com.cvc.cvc_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CvcBackendApplication {
 
